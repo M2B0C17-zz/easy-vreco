20a5b8cc887515b7205016387a3f07351e021d44
macarena baltra <macarena.bc2011@gmail.com>
2017-06-19 23:25:21 -0400

    CSS, JS y HTML

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*Función initMap, con ella inicializamos nuestro mapa, mostrándolo en el div con id map
+	*ZOOM representa el nivel de profundidad de nuestro mapa, entre más zoom más localizado se verá.
+	*CENTER contiene la longitud y altitud en que queremos que se muestre nuestro mapa.
+*/
+
+  function initMap(){
+	  var map = new google.maps.Map(document.getElementById("map"),{
+
+    zoom: 5,
+		center: {lat: -9.1191427, lng: -77.0349046},
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl: false
+	});
+
+	/*Dentro de la función initMap(), agregamos la funcion buscar()
+		*.getCurrentPosition -> permite al usuario obtener su ubicación actual, el parámetro funcionExito,
+		se ejecuta solo cuando el usuario comparte su ubicación, mientras que funcionError se ejecuta
+		cuando se produce un error en la geolocalización.
+	*/
+	function buscar(){
+		if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+		}
+	}
+	document.getElementById("encuentrame").addEventListener("click", buscar);
+	var latitud,longitud;
+
+	/*Agregaremos las variables funcionExito, con el que obtendremos nuestra latitud
+	o longituf y además crearemos un marcador de nuestra ubicación*/
+
+	var funcionExito = function(posicion){
+		latitud = posicion.coords.latitude;
+		longitud = posicion.coords.longitude;
+
+	var miUbicacion = new google.maps.Marker({
+		position: {lat: latitud, lng:longitud},
+		animation: google.maps.Animation.DROP,
+		map: map
+	});
+
+	/*Aumentaremos la profundidad de visualización de nuestro mapa con map.setZoom y le asignaremos
+	un nuevo centro con map.setCenter.
+	También añadimos funcionError con un mensaje para el usuario, en caso de que nuestra geolocalización
+	falle.
+	*/
+
+		map.setZoom(17);
+		map.setCenter({lat: latitud,lng: longitud});
+	}
+
+	var funcionError = function (error){
+		alert("Tenemos un problema con encontrar tu ubicación");
+	}
+}
